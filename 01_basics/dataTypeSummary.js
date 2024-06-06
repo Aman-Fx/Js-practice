37f207d7e861b1fd25471e81fe36caee6387a3ff
Aman Soni <aman.soni@silvergrey.in>
2024-06-06 19:40:36 +0530

    Datatype summary

---
+++ 01_basics/dataTypeSummary.js
@@ -0,0 +1,27 @@
+// Primitive 
+// Non-Primitive
+// is the way how we store data into memory and use it
+
+
+// Primitive = Call by value
+// String
+// Number
+// Boolean
+// Null = Empty
+// undefined
+// Symbol
+// Big-int - used to store big numbers
+
+// Referance type / Non-Primitive
+// Arrays, Objects, Functions
+
+
+// Studying symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log('As id', id);
+// console.log('As anotherId', anotherId);
+
+console.log(id == anotherId);
+
